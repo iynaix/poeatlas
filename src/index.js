625b7c4eef75d360a8ae8ed3b62f7e97ee8e1578
Lin Xianyi <iynaix@gmail.com>
2017-03-14 12:50:49 +0800

    Added redux-perist.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
+import { persistStore } from 'redux-persist'
 import { Provider } from 'react-redux'
+import App from './App'
+
+import './index.css'
 import initStore from './store'
 
 const store = initStore()
 
+class AppContainer extends Component {
+    state = { rehydrated: false }
+
+    componentWillMount() {
+        persistStore(store, {
+            whitelist: ["atlas"],
+        }, () => {
+            this.setState({ rehydrated: true })
+        })
+    }
+
+    render() {
+        if (!this.state.rehydrated) {
+            return null
+        }
+
+        return (
+            <Provider store={store}>
+                <App />
+            </Provider>
+        )
+    }
+}
+
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
+    <AppContainer />,
     document.getElementById('root')
 )
--- src/store.js
+++ src/store.js
@@ -1,9 +1,15 @@
-import { createStore } from 'redux'
+import { createStore, compose } from 'redux'
+import { autoRehydrate } from 'redux-persist'
+
 import rootReducer from  './reducers'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 export default (initialState) =>
     createStore(
         rootReducer,
         initialState,
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+        composeEnhancers(
+            autoRehydrate(),
+        ),
     )
